67d8e45f9901008b98b9ef59aefa9bd614880caf
Austin Bingham <austin.bingham@gmail.com>
2017-12-13 15:29:50 +0100

    Experimenting with pkgutil to do module iteration.

--- ultan/get_names.py
+++ ultan/get_names.py
@@ -2,58 +2,87 @@
 # Given a module name, find all submodules and attributes: IPython.core.compleeterlib.try_import(<module name>)
 # https://docs.python.org/3/library/importlib.html#checking-if-a-module-can-be-imported
 
-from collections import deque
-import importlib
-from importlib.util import find_spec, module_from_spec
-import IPython.core.completerlib as clib
+import ast
+import pkgutil
 
 _cache = None
 
 
-def _walk_namespace(root_names=None):
-    root_names = root_names or clib.get_root_modules()
-    objs = deque((name, None, None) for name in root_names)
+def _find_top_level_names(tree):
+    assert isinstance(tree, ast.Module)
+    for node in tree.body:
+        if isinstance(node, ast.Assign):
+            for target in node.targets:
+                if hasattr(target, 'id'):
+                    yield target.id
+        elif isinstance(node, ast.FunctionDef):
+            yield node.name
+        # TODO: classes, nested stuff
 
-    while objs:
-        name, base_obj, base_name = objs.pop()
-        print('popped', name, base_obj, base_name)
 
-        if base_obj is None:
-            # With no base object, we assume the name refers to a module.
-            # (global objects will get picked up in 'builtins').
+def _walk_namespace():
+    for minfo in pkgutil.walk_packages():
+        spec = minfo.module_finder.find_spec(minfo.name)
+        try:
+            with open(spec.origin, mode='rt') as handle:
+                source = handle.read()
+            tree = ast.parse(source)
+            for name in _find_top_level_names(tree):
+                yield '{}.{}'.format(minfo.name, name)
+        except UnicodeDecodeError:
+            print('unicode decode error: {}'.format(spec.origin))
+        except SyntaxError:
+            print('syntax error: {}'.format(spec.origin))
 
-            # import the module
-            obj = importlib.import_module(name)
+    # with open(path, mode='rt') as handle:
+    #     source = handle.read()
+    # tree = ast.parse(source)
+    # for name in _find_top_level_names(tree):
+    #     yield '{}.{}'.format(the_minfo.name, name)
 
-            # Enqueue each of its submodule and attribute names into objs
-            for subname in clib.try_import(name, False):
-                print('appending', subname, obj, name)
-                objs.append((subname, obj, name))
+    # root_names = root_names or clib.get_root_modules()
+    # objs = deque((name, None, None) for name in root_names)
 
-            yield name
+    # while objs:
+    #     name, base_obj, base_name = objs.pop()
+    #     print('popped', name, base_obj, base_name)
 
-        else:
-            assert base_name
+    #     if base_obj is None:
+    #         # With no base object, we assume the name refers to a module.
+    #         # (global objects will get picked up in 'builtins').
 
-            full_name = '{}.{}'.format(base_name, name)
+    #         # import the module
+    #         obj = importlib.import_module(name)
 
-            print('finding spec', full_name)
-            import_spec = find_spec(full_name)
-            if import_spec:
-                obj = module_from_spec(import_spec)
-                for subname in clib.try_import(full_name):
-                    objs.append((subname, obj, full_name))
+    #         # Enqueue each of its submodule and attribute names into objs
+    #         for subname in clib.try_import(name, False):
+    #             print('appending', subname, obj, name)
+    #             objs.append((subname, obj, name))
 
-            # print('is importable?', base_obj, name)
-            # if clib.is_importable(base_obj, name, True):
-            #     print('importing', name, base_obj)
-            #     obj = importlib.import_module(full_name)
-            #     for subname in clib.try_import(full_name, True):
-            #         print('> appending', subname, object, full_name)
-            #         objs.append((subname, obj, full_name))
-            #     yield full_name
-            # else:
-            #     obj = getattr(base_obj, name)
+    #         yield name
+
+    #     else:
+    #         assert base_name
+
+    #         full_name = '{}.{}'.format(base_name, name)
+
+    #         print('finding spec', full_name)
+    #         import_spec = find_spec(full_name)
+    #         if import_spec:
+    #             obj = module_from_spec(import_spec)
+    #             for subname in clib.try_import(full_name):
+    #                 objs.append((subname, obj, full_name))
+
+    #         # print('is importable?', base_obj, name)
+    #         # if clib.is_importable(base_obj, name, True):
+    #         #     print('importing', name, base_obj)
+    #         #     obj = importlib.import_module(full_name)
+    #         #     for subname in clib.try_import(full_name, True):
+    #         #         print('> appending', subname, object, full_name)
+    #         #         objs.append((subname, obj, full_name))
+    #         #     yield full_name
+    #         # else:
+    #         #     obj = getattr(base_obj, name)
 
 
 def _ensure_cache():
@@ -69,4 +98,4 @@ def get_names(pattern):
             for name in _cache
             if pattern in name)
 
-print(list(_walk_namespace(['http'])))
+# _walk_namespace()
